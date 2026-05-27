0184bb6ac7a571d3c721023ee32f43f9328f8881
lgonzalezl <lgonzalezl@oesia.com>
2026-05-27 09:56:12 +0200

    Fix repository test scrips

--- script1.sh
+++ script1.sh
@@ -6,11 +6,6 @@ echo "=== Comprobando conexión con el repositorio de GitHub ==="
 git remote -v
 
 echo
-echo "=== Actualizando información del repositorio público ==="
-
-git pull
-
-echo
 echo "=== Creando dos ficheros de texto ==="
 
 cat > fichero1.txt << EOF
@@ -26,3 +21,8 @@ También contiene varias líneas.
 Estamos practicando comandos de Linux.
 Este fichero será analizado por el script.
 EOF
+
+echo
+echo "=== Ficheros creados correctamente ==="
+
+ls -l fichero1.txt fichero2.txt
